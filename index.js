228c253abfacbe62f40845f27f9b30dc1c895862
exd0tpy <jsw5258@ajou.ac.kr>
2022-09-15 23:14:35 +0900

    init

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://banda:bandabanda@cluster0.aqaj3o5.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
+    console.log("HELLO");
     const collection = client.db("sample_traning").collection("grades");
     // perform actions on the collection object
     console.log(collection);
