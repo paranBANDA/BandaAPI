af97fa10c07a4660c738ca5449ca6b29f44b70c4
exd0tpy <jsw5258@ajou.ac.kr>
2022-09-15 23:10:52 +0900

    init

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 /*
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://banda:<password>@cluster0.aqaj3o5.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 */
 
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://banda:bandabanda@cluster0.aqaj3o5.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("sample_traning").collection("grades");
+    // perform actions on the collection object
+    console.log(collection);
+    client.close();
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World");
