275b691a4606cc9220bb681cdbed7c626eae8fd8
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-12-06 23:32:21 +0900

    feat(polaroid): byte to base64

--- polaroid/polaroid.js
+++ polaroid/polaroid.js
@@ -57,16 +57,8 @@ router.get('/py', function (req, res) {
 	})
 });
 
-router.post('/uploaddiaryimage', imageUploader.single('image'), (req, res) => {
-	fs.readFile(req.body.image, function(error, data) {
-		if(error){
-		  response.end('500 Internal Server '+error);
-		}else{
-		  // 6. Content-Type 에 4번에서 추출한 mime type 을 입력
-		  response.end(data);
-		  console.log(data)
-		}
-	  });
+router.post('/uploaddiaryimage', imageUploader.single(Buffer.from('image', "base64").toString('utf8')), (req, res) => {
+	console.log(Buffer.from(req.body.image, "base64").toString('utf8'))
 	const picture = req.file.location;
 	const userId = req.body.email;
 	const petId = req.body.petname;
